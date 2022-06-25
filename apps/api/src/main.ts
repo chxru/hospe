60bcb8d28ff51be33ed23372b2d7fd089c59cc4a
Charuka Samarakoon <charuka@protonmail.com>
2022-06-26 02:20:14 +0530

    feat(api): :sparkles: Add swagger docs
    Swagger docs available through localhost:5000/docs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,23 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+
+  const config = new DocumentBuilder()
+    .setTitle('Hospe API')
+    .setDescription('Swagger Documentation for Hospe API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const port = process.env.PORT || 5000;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
 }
 
 bootstrap();
