b7ca4c02d4c9cff3ec61ae5c2ba3df5174dbeae2
mpabu <madhurapabu@gmail.com>
2022-07-01 22:17:59 +0530

    Responsive Sidebar with application header

--- libs/ui/src/lib/layout/sidebar/sidebar.tsx
+++ libs/ui/src/lib/layout/sidebar/sidebar.tsx
@@ -1,12 +1,15 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import {
   Navbar,
   Group,
-  Code,
   ScrollArea,
   createStyles,
   Avatar,
   Text,
+  MediaQuery,
+  Burger,
+  AppShell,
+  Header,
 } from '@mantine/core';
 import { LinksGroup, LinksGroupProps } from './linkGroup';
 import { ChevronRight } from 'tabler-icons-react';
@@ -80,43 +83,62 @@ export const Sidebar: FC<SiderbarProps> = ({ data }) => {
   const { classes } = useStyles();
   const links = data.map((item) => <LinksGroup {...item} key={item.label} />);
 
+  const [opened, setOpened] = useState(false);
+
   return (
-    <Navbar height={'800'} width={{ sm: 300 }} p="md" className={classes.navbar}  >
-      <Navbar.Section className={classes.header}  mt="xs">
-        <Group position="apart">
-          <Logo width={120} />
-          <Code sx={{ fontWeight: 700 }}>v3.1.2</Code>
-        </Group>
-      </Navbar.Section>
+    <div>
+      <AppShell navbarOffsetBreakpoint="sm" fixed
 
-      <Navbar.Section className={classes.links}  mx="-xs" px="xs" >
-        <ScrollArea style={{ height: 420 }}>
-          <div className={classes.linksInner}>{links}</div>
-        </ScrollArea>
-      </Navbar.Section>
+        navbar={
+          <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ xs: 300 }} height={'100%'} className={classes.navbar}>
 
-      <Navbar.Section className={classes.footer}>
-        <Group>
-          <Avatar
-            src={
-              'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80'
-            }
-            radius="xl"
-          />
+            <Navbar.Section className={classes.links} mx="-xs" px="xs" >
+              <ScrollArea style={{ height: 420 }}>
+                <div className={classes.linksInner}>{links}</div>
+              </ScrollArea>
+            </Navbar.Section>
 
-          <div style={{ flex: 1 }}>
-            <Text size="sm" weight={500}>
-              Ann Nullpointer
-            </Text>
+            <Navbar.Section className={classes.footer}>
+              <Group>
+                <Avatar
+                  src={
+                    'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80'
+                  }
+                  radius="xl"
+                />
 
-            <Text color="dimmed" size="xs">
-              anullpointer@yahoo.com
-            </Text>
-          </div>
+                <div style={{ flex: 1 }}>
+                  <Text size="sm" weight={500}>
+                    Ann Nullpointer
+                  </Text>
 
-          <ChevronRight size={14} />
-        </Group>
-      </Navbar.Section>
-    </Navbar>
+                  <Text color="dimmed" size="xs">
+                    anullpointer@yahoo.com
+                  </Text>
+                </div>
+
+                <ChevronRight size={14} />
+              </Group>
+            </Navbar.Section>
+          </Navbar>
+        }
+
+        header={
+          <Header height={70} p="md">
+            <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+
+              <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+                <Burger    opened={opened} onClick={() => setOpened((o) => !o)} size="sm" mr="xl"/>
+              </MediaQuery>
+
+              <Group position="apart">
+                <Logo width={120} />
+              </Group>
+            </div>
+          </Header>
+        }
+      >
+      </AppShell>
+    </div>
   );
 };
