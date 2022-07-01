23c53dc8836061fc222e324655899b0e694186eb
Pasan Narangoda <narangodapasan@gmail.com>
2022-07-01 23:35:09 +0530

    format setting

--- libs/ui/src/lib/components/searchbar/searchbar.tsx
+++ libs/ui/src/lib/components/searchbar/searchbar.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   TextInput,
   Button,
@@ -10,93 +10,109 @@ import {
   ActionIcon,
   useMantineTheme,
   NativeSelect,
-  Center} from '@mantine/core';
+  Center,
+} from '@mantine/core';
 import {
   ArrowRight,
   ArrowLeft,
   ArrowDownCircle,
-  Search } from 'tabler-icons-react';
+  Search,
+} from 'tabler-icons-react';
 import { DatePicker } from '@mantine/dates';
 
-export const Searchbar = ({...props}) => {
-
+export const Searchbar = ({ ...props }) => {
   const [opened, setOpen] = useState(false);
   const theme = useMantineTheme();
 
   return (
     <Container>
-
       <Box>
-        <Grid sx={(theme) =>({backgroundColor: 'lightblue',
-          padding: theme.spacing.xl,
-          borderRadius: theme.radius.md,})}>
-
+        <Grid
+          sx={(theme) => ({
+            backgroundColor: 'lightblue',
+            padding: theme.spacing.xl,
+            borderRadius: theme.radius.md,
+          })}
+        >
           <Grid.Col md={12}>
-          <TextInput
-            icon={<Search size={18} />}
-            radius="xl"
-            size="md"
-            rightSection={
-              <ActionIcon size={32} radius="xl" color={theme.primaryColor} variant="filled">
-                {theme.dir === 'ltr' ? <ArrowRight size={18} /> : <ArrowLeft size={18} />}
-              </ActionIcon>
-            }
-            placeholder="Search Doctors/Specialization"
-            rightSectionWidth={42}
-            {...props}
-          />
+            <TextInput
+              icon={<Search size={18} />}
+              radius="xl"
+              size="md"
+              rightSection={
+                <ActionIcon
+                  size={32}
+                  radius="xl"
+                  color={theme.primaryColor}
+                  variant="filled"
+                >
+                  {theme.dir === 'ltr' ? (
+                    <ArrowRight size={18} />
+                  ) : (
+                    <ArrowLeft size={18} />
+                  )}
+                </ActionIcon>
+              }
+              placeholder="Search Doctors/Specialization"
+              rightSectionWidth={42}
+              {...props}
+            />
           </Grid.Col>
-
         </Grid>
       </Box>
 
       <Divider m="xs" label="Advance Search" labelPosition="center" />
       <Center>
-        <ActionIcon variant="transparent" color="blue" onClick={() => setOpen((o) => !o)}>
-          <ArrowDownCircle/>
+        <ActionIcon
+          variant="transparent"
+          color="blue"
+          onClick={() => setOpen((o) => !o)}
+        >
+          <ArrowDownCircle />
         </ActionIcon>
       </Center>
 
-      <Box m='xs'>
+      <Box m="xs">
         <Collapse in={opened}>
-          <Grid sx={(theme) =>({backgroundColor: 'lightblue',
+          <Grid
+            sx={(theme) => ({
+              backgroundColor: 'lightblue',
               padding: theme.spacing.xl,
-              borderRadius: theme.radius.md,})}>
-            <Grid.Col md={3} >
+              borderRadius: theme.radius.md,
+            })}
+          >
+            <Grid.Col md={3}>
               <NativeSelect
                 data={['React', 'Vue', 'Angular', 'Svelte']}
                 placeholder="Sepecialization"
                 radius="xl"
               />
             </Grid.Col>
-            <Grid.Col md={3} >
+            <Grid.Col md={3}>
               <NativeSelect
                 data={['React', 'Vue', 'Angular', 'Svelte']}
                 placeholder="Gender"
                 radius="xl"
               />
             </Grid.Col>
-            <Grid.Col md={3} >
+            <Grid.Col md={3}>
               <NativeSelect
                 data={['React', 'Vue', 'Angular', 'Svelte']}
                 placeholder="Session Time"
                 radius="xl"
               />
             </Grid.Col>
-            <Grid.Col md={3} >
-              <DatePicker
-                placeholder="Pick date"
-                radius="xl"/>
+            <Grid.Col md={3}>
+              <DatePicker placeholder="Pick date" radius="xl" />
             </Grid.Col>
             <Grid.Col md={12}>
               <Center>
                 <Button> Search </Button>
               </Center>
             </Grid.Col>
-
           </Grid>
         </Collapse>
       </Box>
     </Container>
   );
-}
+};
