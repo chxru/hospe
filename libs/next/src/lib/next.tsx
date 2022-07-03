5987d02ec1cf3e61969bd0bf61748c2bde0353b1
Charuka Samarakoon <charuka@protonmail.com>
2022-07-03 15:53:10 +0530

    feat(next-auth): :sparkles: Next common lib init

---
+++ libs/next/src/lib/next.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface NextProps {}
+
+export function Next(props: NextProps) {
+  return (
+    <div>
+      <h1>Welcome to Next!</h1>
+    </div>
+  );
+}
+
+export default Next;
