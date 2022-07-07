8a5567c97b7d7f6fe5a8513847809fe3eb02d925
Pasan Narangoda <narangodapasan@gmail.com>
2022-07-08 03:12:55 +0530

    feat(api): :sparkles: create db schema for channeling

--- apps/api/src/app/channeling/entities/channeling.entity.ts
+++ apps/api/src/app/channeling/entities/channeling.entity.ts
@@ -1,1 +1,12 @@
-export class Channeling {}
+import * as mongoose from 'mongoose';
+
+export const ChannelingEntity = new mongoose.Schema({
+  channelingId: String,
+  channelingPatientId: String,
+  channelingDoctorId: String,
+  channelingType: String,
+  channelingDate: Date,
+  channelingTime: String,
+  channelingDescription: String,
+  channelingStatus: String,
+})
