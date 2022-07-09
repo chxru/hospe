b9d3941d32c3287b4c1ed0f8c9f2ba01f56261cc
Charuka Samarakoon <charuka@protonmail.com>
2022-07-09 15:00:49 +0530

    refactor(firebase-web): :fire: Remove firebase

--- apps/frontend/user/pages/_app.tsx
+++ apps/frontend/user/pages/_app.tsx
@@ -1,28 +1,10 @@
-import { useEffect, useRef } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
 import { MantineProvider } from '@mantine/core';
 
-import { initFirebase } from 'firebase-web';
-import type { FirebaseOptions } from 'firebase/app';
-
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
-
-  const firebaseConfig = useRef<FirebaseOptions>({
-    apiKey: 'AIzaSyB4q3Wq7r9mLfVmPVEYfqcCdBMLMqFrx5o',
-    authDomain: 'hospe-9ca9e.firebaseapp.com',
-    projectId: 'hospe-9ca9e',
-    storageBucket: 'hospe-9ca9e.appspot.com',
-    messagingSenderId: '1072809987003',
-    appId: '1:1072809987003:web:741a372f5879a5a39d529e',
-  });
-
-  useEffect(() => {
-    initFirebase(firebaseConfig.current);
-  }, []);
-
   return (
     <>
       <Head>
--- libs/firebase/web/README.md
+++
@@ -1,7 +0,0 @@
-# firebase-web
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test firebase-web` to execute the unit tests via [Jest](https://jestjs.io).
