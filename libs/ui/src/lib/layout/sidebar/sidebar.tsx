5a8b46df6ec629d8bc3577b1117a8edc29bfc8c2
Pasan Narangoda <narangodapasan@gmail.com>
2022-08-19 02:42:39 +0530

    style(ui): :art: remove unused mantime components

--- libs/ui/src/lib/layout/sidebar/sidebar.tsx
+++ libs/ui/src/lib/layout/sidebar/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Children, FC, ReactNode, useState } from 'react';
+import { FC, ReactNode, useState } from 'react';
 import {
   Navbar,
   Group,
@@ -119,7 +119,7 @@ export const Sidebar: FC<SiderbarProps> = ({
                   src={
                     'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80'
                   }
-                  radius="xs"
+                  radius="xl"
                 />
 
                 <div style={{ flex: 1 }}>
