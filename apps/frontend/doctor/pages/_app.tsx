fbbafaf9260ea8896be097e0861308f27f16f701
Pasan Narangoda <narangodapasan@gmail.com>
2022-08-19 18:02:50 +0530

    feat(ui): :sparkles: add sidebard to Doc dashboard

--- apps/frontend/doctor/pages/_app.tsx
+++ apps/frontend/doctor/pages/_app.tsx
@@ -1,9 +1,61 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import {
+  Gauge,
+  Notes,
+  CalendarStats,
+  PresentationAnalytics,
+  FileAnalytics,
+  Adjustments,
+  Lock,
+} from 'tabler-icons-react';
+import { MantineProvider, Title, Group } from '@mantine/core';
+import { Sidebar } from '@hospe/ui';
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
+  const mockData = {
+    title: 'Mantine',
+    username: 'John Doe',
+    email: 'john@gmail.com',
+    data: [
+      { label: 'Dashboard', icon: Gauge },
+      {
+        label: 'Create Session',
+        icon: Notes,
+        initiallyOpened: true,
+        links: [
+          { label: 'Overview', link: '/' },
+          { label: 'Forecasts', link: '/' },
+          { label: 'Outlook', link: '/' },
+          { label: 'Real time', link: '/' },
+        ],
+      },
+      {
+        label: 'Appointments',
+        icon: CalendarStats,
+        links: [
+          { label: 'Upcoming releases', link: '/' },
+          { label: 'Previous releases', link: '/' },
+          { label: 'Releases schedule', link: '/' },
+        ],
+      },
+      { label: 'Analytics', icon: PresentationAnalytics },
+      { label: 'Contracts', icon: FileAnalytics },
+      { label: 'Settings', icon: Adjustments },
+      {
+        label: 'Security',
+        icon: Lock,
+        links: [
+          { label: 'Enable 2FA', link: '/' },
+          { label: 'Change password', link: '/' },
+          { label: 'Recovery codes', link: '/' },
+        ],
+      },
+    ],
+    children: undefined,
+  };
+
   return (
     <>
       <Head>
@@ -22,7 +74,9 @@ export default function App(props: AppProps) {
           colorScheme: 'light',
         }}
       >
-        <Component {...pageProps} />
+        <Sidebar {...mockData}>
+          <Component {...pageProps} />
+        </Sidebar>
       </MantineProvider>
     </>
   );
