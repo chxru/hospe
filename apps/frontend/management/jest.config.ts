a81272216d50d6c1b56a5153fdde5564d733d166
Maneesha Nethmini <maneesha.nethmini96@gmail.com>
2022-08-20 05:10:04 +0530

    feat(ui): :zap: init management app

---
+++ apps/frontend/management/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'frontend-management',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/apps/frontend/management',
+};
