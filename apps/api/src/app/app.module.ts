13697d144f7e64258b38b94cf5ebacfd4d2c995d
Charuka Samarakoon <charuka@protonmail.com>
2022-08-19 23:37:16 +0530

    feat(api): :sparkles: Add role based access control

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as redisStore from 'cache-manager-redis-store';
+import { ClientOpts } from 'redis';
 
-import { ChannelingModule } from './channeling/channeling.module';
+import { RoleGuard } from './auth/rbac/role.guard';
+import {
+  RefreshToken,
+  RefreshTokenSchema,
+} from './auth/token/schema/token.schema';
+
 import { AuthModule } from './auth/auth.module';
+import { ChannelingModule } from './channeling/channeling.module';
 import { EmployeeModule } from './employee/employee.module';
+import { TokenService } from './auth/token/token.service';
 
 @Module({
   imports: [
+    CacheModule.register<ClientOpts>({
+      store: redisStore,
+      host: process.env.REDIS_URL,
+      port: Number(process.env.REDIS_PORT),
+      auth_pass: process.env.REDIS_AUTH_PASS,
+    }),
     MongooseModule.forRoot(process.env.MONGO_URL),
+    MongooseModule.forFeature([
+      { name: RefreshToken.name, schema: RefreshTokenSchema },
+    ]),
     AuthModule,
     ChannelingModule,
     EmployeeModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    TokenService,
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
 export class AppModule {}
