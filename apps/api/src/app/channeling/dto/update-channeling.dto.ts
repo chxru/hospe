fbc733418569f52351676ea5af3281e7deebd47a
Charuka Samarakoon <charuka@protonmail.com>
2022-12-22 07:52:09 +0530

    refactor(api): :fire: removed nestjs api

--- apps/api/src/app/channeling/dto/update-channeling.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateChannelingDto } from './create-channeling.dto';
-
-export class UpdateChannelingDto extends PartialType(CreateChannelingDto) {}
--- apps/api/src/services/bcrypt.service.ts
+++
@@ -1,5 +0,0 @@
-import { compare, hash } from 'bcrypt';
-
-export const hashPassword = async (password: string) => hash(password, 10);
-export const comparePassword = async (password: string, hashed: string) =>
-  compare(password, hashed);
