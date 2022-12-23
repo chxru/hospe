71bc27d7b28f99eae5cf0ec4c5c5482ddb039e6e
Pasan Narangoda <narangodapasan@gmail.com>
2022-12-23 21:05:16 +0530

    feat(types): :sparkles: added doctor.ts

---
+++ libs/next/src/api/doctor.ts
@@ -0,0 +1,18 @@
+import { instance } from './axios';
+
+interface CreateSessionProps {
+  docId: string;
+  docName: string;
+  date: string;
+  time: string;
+  maximumPatients: number;
+  doctorFee: number;
+}
+
+const CreateSession = (props: CreateSessionProps) => {
+  return instance.post('/channeling/create-channeling', props).then((res) => res.data);
+};
+
+export const Doctor = {
+  CreateSession
+};
--- libs/next/src/api/index.ts
+++ libs/next/src/api/index.ts
@@ -1,7 +1,9 @@
 import { Auth } from './auth';
 import { Employee } from './employee';
+import { Doctor } from './doctor';
 
 export const Api = {
   Auth,
   Employee,
+  Doctor,
 };
