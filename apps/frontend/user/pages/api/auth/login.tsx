728f72061886d13143339a15ecc8d79b4633ca47
Charuka Samarakoon <charuka@protonmail.com>
2022-12-24 05:19:25 +0530

    fix: :bug: next internal api not using env var

--- apps/frontend/user/pages/api/auth/login.tsx
+++ apps/frontend/user/pages/api/auth/login.tsx
@@ -3,9 +3,11 @@ import axios from 'axios';
 import { setCookie } from 'cookies-next';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+const API_URL = process.env.API_URL || 'http://localhost:5000';
+
 const LoginHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await axios
-    .post<UserLoginRes>('http://localhost:5000/auth/login', req.body)
+    .post<UserLoginRes>(`${API_URL}/auth/login`, req.body)
     .then((r) => r.data);
 
   setCookie('refresh-cookie', response.tokens.refresh.value, {
