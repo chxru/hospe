00fd7cc171de8bb990786084ccc2ca55783a1e00
Charuka Samarakoon <charuka@protonmail.com>
2022-12-23 22:08:12 +0530

    fix: :bug: centralized types

--- libs/next/src/api/doctor.ts
+++ libs/next/src/api/doctor.ts
@@ -1,18 +1,12 @@
+import { ISessionForm } from '@hospe/types';
 import { instance } from './axios';
 
-interface CreateSessionProps {
-  docId: string;
-  docName: string;
-  date: string;
-  time: string;
-  maximumPatients: number;
-  doctorFee: number;
-}
-
-const CreateSession = (props: CreateSessionProps) => {
-  return instance.post('/channeling/create-channeling', props).then((res) => res.data);
+const CreateSession = (props: ISessionForm) => {
+  return instance
+    .post('/channeling/create-channeling', props)
+    .then((res) => res.data);
 };
 
 export const Doctor = {
-  CreateSession
+  CreateSession,
 };
