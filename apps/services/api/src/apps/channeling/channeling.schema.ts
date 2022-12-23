012c5d122509ee717b4c53c98fd2965afe3bbf3f
Pasan Narangoda <narangodapasan@gmail.com>
2022-12-23 12:24:58 +0530

    refactor(api): :recycle: change data type
    change Date data type into string, in order to get the date in dd-mm-yyyy format

--- apps/services/api/src/apps/channeling/channeling.schema.ts
+++ apps/services/api/src/apps/channeling/channeling.schema.ts
@@ -1,7 +1,7 @@
 import { Schema } from 'mongoose';
 
 export const ChannelingSchema = new Schema({
-  date: { type: Date, required: true, index: -1, default: Date.now },
+  date: { type: String, required: true, index: -1, default: Date.now },
   time: { type: String, required: true, index: -1 },
   maximumPatients: { type: Number, required: true },
   doctorFee: { type: Number, required: true },
