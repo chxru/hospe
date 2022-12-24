40058d245931f381c058c42ecb96ffe0b7a53f7f
Charuka Samarakoon <charuka@protonmail.com>
2022-12-24 06:40:11 +0530

    feat(user): :sparkles: add user register

--- libs/next/src/api/index.ts
+++ libs/next/src/api/index.ts
@@ -1,9 +1,11 @@
 import { Auth } from './auth';
 import { Employee } from './employee';
 import { Doctor } from './doctor';
+import { User } from './user';
 
 export const Api = {
   Auth,
   Employee,
   Doctor,
+  User,
 };
