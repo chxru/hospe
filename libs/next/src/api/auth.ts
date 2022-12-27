d3ce3959bf0f677f445525d3d7890c829e8b62a2
Charuka Samarakoon <charuka@protonmail.com>
2022-12-27 14:41:06 +0530

    fix(api): :bug: auth calls not taking absolute path

--- libs/next/src/api/auth.ts
+++ libs/next/src/api/auth.ts
@@ -3,12 +3,12 @@ import axios from 'axios';
 
 const Login = (body: UserLoginReq) => {
   return axios
-    .post<UserLoginRes>('api/auth/login', body)
+    .post<UserLoginRes>('/api/auth/login', body)
     .then((res) => res.data);
 };
 
 const RefreshToken = (): Promise<TokenRefreshRes> => {
-  return axios.post('api/auth/refresh').then((res) => res.data);
+  return axios.post('/api/auth/refresh').then((res) => res.data);
 };
 
 export const Auth = {
